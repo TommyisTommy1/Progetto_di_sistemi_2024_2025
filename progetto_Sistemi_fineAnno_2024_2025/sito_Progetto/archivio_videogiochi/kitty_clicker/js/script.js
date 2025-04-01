3a470a14debfcf9305617d1d2a597a1fd9e1ed86
Tommaso <tomferra07@gmail.com>
2025-04-01 15:31:55 +0200

    aggiunto kitty decenti

--- progetto_Sistemi_fineAnno_2024_2025/sito_Progetto/archivio_videogiochi/kitty_clicker/js/script.js
+++ progetto_Sistemi_fineAnno_2024_2025/sito_Progetto/archivio_videogiochi/kitty_clicker/js/script.js
@@ -1,6 +1,40 @@
 
-document.getElementById("kittyUso").addEventListener("click", function(){
+let shop = "";
+
+for (let i = 0; i < kittyShop.length; i++) {
+    shop += "<td class='kitty'>";
+
+    shop += '<img src="img/' + kittyShop[i].immagine + '" alt=' + kittyShop[i].alt + '/>';
+    shop += '<h3>' + kittyShop[i].nome +'</h3>';
+    shop += '<p>Costo: <span class="costo">' + kittyShop[i].costo + '</span></p>';
+    shop += '<p>Punteggio: <span class="punteggio">' + kittyShop[i].punteggio + '</span></p>';
+    shop += '<div class="inUso">';
+    if(kittyShop[i].acquistato == false){
+        shop += '<input type="button" class="acquista" value="Acquista"></input>';
+    }
+    shop += '</div>';
+            
+    shop += "</td>";
+}
+
+shop += '<td class="kitty" style="background-color: seagreen;"><h3>Coming <br> Soon</h3></td>';
+document.getElementById("shopKitty").innerHTML = shop;
+
+
+document.getElementById("bottoneKittyUso").addEventListener("click", function(){
     punteggioTotale += parseFloat(document.getElementById("kittyUso").value);
    
     document.getElementById("valorePunteggio").innerHTML = punteggioTotale;
+});
+
+
+document.getElementById("compraBonus").addEventListener("click", function(){
+    if(punteggioTotale >= 1000){
+        let bonus = Math.floor(Math.random() * 100) + 250;
+        punteggioTotale -= bonus;
+        punteggioTotale += bonus;
+        document.getElementById("valorePunteggio").innerHTML = punteggioTotale;
+    }else{
+        alert("Non hai abbastanza punteggio!");
+    }
 });
--- progetto_Sistemi_fineAnno_2024_2025/sito_Progetto/archivio_videogiochi/kitty_clicker/js/variabili.js
+++ progetto_Sistemi_fineAnno_2024_2025/sito_Progetto/archivio_videogiochi/kitty_clicker/js/variabili.js
@@ -1,1 +1,67 @@
 var punteggioTotale = 0;
+
+let kittyShop = 
+[
+    {
+        nome: "Mini kitty",
+        immagine: "mini_kitty.png",
+        alt: "mini_kitty",
+        costo: 0,
+        punteggio: 1,
+        acquistato: true
+    },
+
+    {
+        nome: "Kitty",
+        immagine: "kitty.png",
+        alt: "kitty",
+        costo: 30,
+        punteggio: 2,
+        acquistato: false
+    },
+
+    {
+        nome: "Grande kitty",
+        immagine: "grande_kitty.png",
+        alt: "grande_kitty",
+        costo: 100,
+        punteggio: 5,
+        acquistato: false
+    },
+
+    {
+        nome: "Bullo kitty",
+        immagine: "bullo_kitty.png",
+        alt: "bullo_kitty",
+        costo: 500,
+        punteggio: 7,
+        acquistato: false
+    },
+
+    {
+        nome: "Mago kitty",
+        immagine: "mago_kitty.png",
+        alt: "mago_kitty",
+        costo: 1000,
+        punteggio: 10,
+        acquistato: false
+    },
+
+    {
+        nome: "Angelo kitty",
+        immagine: "angelo_kitty.png",
+        alt: "angelo_kitty",
+        costo: 2000,
+        punteggio: 15,
+        acquistato: false
+    },
+
+    {
+        nome: "Demone kitty",
+        immagine: "demone_kitty.png",
+        alt: "demone_kitty",
+        costo: 10000,
+        punteggio: 25,
+        acquistato: false
+    }
+];
